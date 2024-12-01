c38af9623bfe181ddc559b25010ce9fcc2432c20
Janmejay <abnjain25@gmail.com>
2024-12-02 04:53:08 +0530

    removed hostname

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
-const hostname = '127.0.0.1';
+const hostname = process.env.HOSTNAME || '127.0.0.1';
 const port = process.env.PORT || 3004;
 const path = require("path")
 
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
 
 // Creating the server
 const server = http.createServer(app);
-server.listen(port, hostname, () => {
+server.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
   });
